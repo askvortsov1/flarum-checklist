f049c5a51b7812a351b5eee085fc44a085ae4961
Alexander Skvortsov <sasha.skvortsov109@gmail.com>
2021-02-18 20:45:57 -0500

    Update post by clicking on checkbox

--- js/src/forum/index.js
+++ js/src/forum/index.js
@@ -1,3 +1,39 @@
+import { extend } from 'flarum/extend';
+import CommentPost from 'flarum/components/CommentPost';
+
 app.initializers.add('askvortsov/flarum-checklist', () => {
-  console.log('[askvortsov/flarum-checklist] Hello, forum!');
+  function toggleCheckbox(post, remainingCheckboxes, checked) {
+    let match;
+    const checkListRegex = /([\-\*]|[0-9]+\.) (\[[ xX]\])/g;
+    const currContent = post.content();
+
+    while ((match = checkListRegex.exec(currContent))) {
+      if (remainingCheckboxes === 0) {
+        const valIndex = match.index + match[0].length - 2;
+        let content = currContent.substring(0, valIndex) + (checked ? 'x' : ' ') + currContent.substring(valIndex + 1);
+
+        post.save({ content }).then(() => {
+          m.redraw();
+        });
+        return;
+      }
+      remainingCheckboxes--;
+    }
+  }
+
+  function processChecklists() {
+    const post = this.attrs.post;
+    if (!post.canEdit()) return;
+
+    this.$('input[type="checkbox"]').each(function (i, element) {
+      element.disabled = false;
+
+      element.onchange = () => {
+        toggleCheckbox(post, i, element.checked);
+      };
+    });
+  }
+
+  extend(CommentPost.prototype, 'oncreate', processChecklists);
+  extend(CommentPost.prototype, 'onupdate', processChecklists);
 });
